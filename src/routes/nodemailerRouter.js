012b36c0f18780be8e438d1c9dbf8fc3aa688a85
santicasalis <santicasalis@hotmail.com>
2023-12-01 22:58:34 -0300

    si

--- src/routes/nodemailerRouter.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const { Router } = require("express");
-const nodemailerRouter = Router();
-
-const welcomeHandler = require("../handlers/nodemailer/welcomeHandler")
-
-nodemailerRouter.post("/welcome", welcomeHandler)
-
-module.exports = nodemailerRouter;
