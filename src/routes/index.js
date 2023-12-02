faadf943b49f537c9e49e85e101cee9fcc4409c1
santicasalis <santicasalis@hotmail.com>
2023-12-02 11:13:06 -0300

    pruebo customer

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,13 @@ const { Router } = require("express");
 const router = Router();
 
 const tattooArtistsRouter = require("./tattooArtistRouter");
-// const customerRouter = require("./customerRouter");
+const customerRouter = require("./customerRouter");
 const publicationRouter = require("./publicationRouter");
 // const PriceRangeRouter = require("./priceRangeRouter");
 const tattoStyleRouter = require("./tattooStyleRouter");
 // const filterRouter = require("./filterRouter");
 // const timeAvailabilityRouter = require("./timeAvailabilityRouter");
-// const appointmentRouter = require("./appointmentRouter");
+const appointmentRouter = require("./appointmentRouter");
 // const loginRouter = require("./loginRouter");
 
 // const timeAvailabilityExceptionRouter = require("./timeAvailabilityExceptionRouter");
@@ -19,11 +19,11 @@ router.use("/tattooStyles", tattoStyleRouter);
 router.use("/tattooArtists", tattooArtistsRouter);
 // router.use("/priceRange", PriceRangeRouter);
 router.use("/publications", publicationRouter);
-// router.use("/customers", customerRouter);
+router.use("/customers", customerRouter);
 // router.use("/filters", filterRouter);
 // router.use("/timeAvailabilities", timeAvailabilityRouter);
 // router.use("/timeAvailabilityExceptions", timeAvailabilityExceptionRouter);
-// router.use("/appointments", appointmentRouter);
+router.use("/appointments", appointmentRouter);
 // router.use("/auth", loginRouter);
 
 // router.use("/admins", adminRouter);
