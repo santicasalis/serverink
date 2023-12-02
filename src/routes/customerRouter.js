e2107792a26ed14a889f8b8519f1086b194f7ddb
santicasalis <santicasalis@hotmail.com>
2023-12-01 23:38:23 -0300

    customer??

--- src/routes/customerRouter.js
+++ src/routes/customerRouter.js
@@ -1,17 +1,15 @@
-const { Router } = require('express')
-const customerRouter = Router()
-const createCustomerHandler = require('../handlers/customerHandlers/createCustomerHandler')
-const putCustomerHandler = require('../handlers/customerHandlers/updateCustomerHandler')
-const deleteCustomerHandler = require('../handlers/customerHandlers/deleteCustomerHandler')
-const getCustomersHandler = require('../handlers/customerHandlers/getCustomersHandler')
-const getCustomerByIdHandler = require('../handlers/customerHandlers/getCustomerByIdHandler')
+const { Router } = require("express");
+const customerRouter = Router();
+const createCustomerHandler = require("../handlers/CustomerHandlers/createCustomerHandler");
+const putCustomerHandler = require("../handlers/customerHandlers/updateCustomerHandler");
+const deleteCustomerHandler = require("../handlers/customerHandlers/deleteCustomerHandler");
+const getCustomersHandler = require("../handlers/customerHandlers/getCustomersHandler");
+const getCustomerByIdHandler = require("../handlers/customerHandlers/getCustomerByIdHandler");
 
+customerRouter.get("/", getCustomersHandler);
+customerRouter.get("/:id", getCustomerByIdHandler);
+customerRouter.post("/", createCustomerHandler);
+customerRouter.put("/:id", putCustomerHandler);
+customerRouter.delete("/:id", deleteCustomerHandler); //
 
-customerRouter.get('/', getCustomersHandler)
-customerRouter.get('/:id', getCustomerByIdHandler)
-customerRouter.post('/', createCustomerHandler)
-customerRouter.put('/:id', putCustomerHandler)
-customerRouter.delete('/:id', deleteCustomerHandler)//
-
-
-module.exports = customerRouter
+module.exports = customerRouter;
