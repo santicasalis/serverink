910c242604ee4bb81f964690130c064d846e6d22
santicasalis <santicasalis@hotmail.com>
2023-12-11 14:13:46 -0300

    MODIFICACIONES MP Y EMAIL

--- src/utils/nodemailer/confirm-reservation-artist.js
+++ src/utils/nodemailer/confirm-reservation-artist.js
@@ -1,5 +1,5 @@
 module.exports = `
 <h1>Tienes una nueva solicitud de turno</h1>
 <p>Confirmalo o deniegalo en el siguiente enlace</p>
-<a href="http://localhost:3000/ruta de pago">Pagar turno</a>
-`
+<a href="https://connectink.vercel.app/ruta de pago">Pagar turno</a>
+`;
--- src/utils/nodemailer/welcome.js
+++ src/utils/nodemailer/welcome.js
@@ -5,7 +5,7 @@ const welcome = (name) => {
         <p>Somos tu plataforma de referencia para descubrir talentosos tatuadores y agendar tus próximos tatuajes.</p>
         <p>Gracias por confiar en nosotros para tu experiencia de tatuaje única.</p>
         <p>Explora nuestra selección de tatuadores y descubre su arte excepcional. Cuando encuentres al artista perfecto, ¡agenda tu turno con facilidad!</p>
-        <a href="http://localhost:3000">Ingresa aquí para comenzar tu viaje en ConnectInk</a>
+        <a href="https://connectink.vercel.app/">Ingresa aquí para comenzar tu viaje en ConnectInk</a>
       `;
 };
 
