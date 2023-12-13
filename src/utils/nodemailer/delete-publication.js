fd2e9543904e423acfb12aa5425f945aac46c98f
santicasalis <santicasalis@hotmail.com>
2023-12-12 22:35:05 -0300

    plantillas nodemailer

--- src/utils/nodemailer/delete-publication.js
+++ src/utils/nodemailer/delete-publication.js
@@ -2,4 +2,5 @@ module.exports = `
 <h1>Tu publicacion fue eliminada</h1>
 <h3>Hemos decidido eliminar tu publicación porque infringe con las políticas de usuario aceptadas al crear la cuenta</h3>
 <p>Si crees que es un fallo, puedes reclamar en el siguiente enlace, proporcionando el id de la publicacion adjuntado a continuacion</p>
-`
+<h3>Puedes comunicarte por mail a connectinkproject@gmail.com</h3>
+`;
