7ced9fe98d53f87a368ef3dbf4fb37f64328cb7b
santicasalis <santicasalis@hotmail.com>
2023-12-13 23:39:29 -0300

    review handler

--- src/handlers/reviewHandlers/createReviewHandler.js
+++ src/handlers/reviewHandlers/createReviewHandler.js
@@ -1,19 +1,28 @@
 const createReview = require("../../controllers/reviewControllers/createReview");
 
 const createReviewHandler = async (req, res) => {
-    const { artistId, customerId, comment, image, rating } = req.body;
-    try {
-        const newCreateReview = await createReview(
-            artistId,
-            customerId,
-            comment,
-            image,
-            rating
-        );
-        res.status(201).json(newCreateReview);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
+  const { tattooArtistId, customerId, appointmentId, comment, image, rating } =
+    req.body;
+  try {
+    const newReview = await createReview({
+      tattooArtistId,
+      customerId,
+      appointmentId,
+      comment,
+      image,
+      rating,
+    });
+
+    if (newReview.code === 201) {
+      res
+        .status(201)
+        .json({ message: newReview.message, data: newReview.data });
+    } else {
+      res.status(newReview.code).json({ error: newReview.error });
     }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 module.exports = createReviewHandler;
